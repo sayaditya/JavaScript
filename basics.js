7c22ad224b2a467c9ab09c52b86f3f657fb7c5c6
Aditya Patel <96697492+sayaditya@users.noreply.github.com>
2024-09-29 09:45:18 +0000

    data type

--- basics.js
+++ basics.js
@@ -1,1 +1,14 @@
-console.log("hello");
+"use strict"; // treat all Js code as newer version
+// alert (3+3) // we are using nodejs , not browser
+
+// data type 
+let name = "aditya";
+let age  = 21;
+let isLoggedIn = false;
+let state;
+
+// null => standalone value 
+// undefined => value is not defined 
+// typeof => gives the type of any data type 
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
