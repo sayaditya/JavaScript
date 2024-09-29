fb1176ddb588de860366e3aeadd85b6acf512367
Aditya Patel <96697492+sayaditya@users.noreply.github.com>
2024-09-29 11:31:10 +0000

    updated

---
+++ stack.js
@@ -0,0 +1,6 @@
+// Stack ( primitive )
+// we get a copy of that value
+
+// Heap ( Non- primitive)
+// we get the original value 
+
---
+++ strings.js
@@ -0,0 +1,5 @@
+const name = "aditya";
+const repoCount = 5;
+
+// console.log(name+repoCount+" Value "); //this is outdated
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
