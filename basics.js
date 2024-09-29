d8139cc26adc1c0930efedcfdcbd1b520e4a214b
Aditya Patel <96697492+sayaditya@users.noreply.github.com>
2024-09-29 11:02:13 +0000

    basics completed

--- basics.js
+++ basics.js
@@ -2,13 +2,60 @@
 // alert (3+3) // we are using nodejs , not browser
 
 // data type 
-let name = "aditya";
-let age  = 21;
-let isLoggedIn = false;
-let state;
+// 1) primitive 
+    // 7 types : string , number , boolean, null,undefined,symbol,BigInt
+// 2) reference (non primitive)
+    // Array, Objects, Functions
+
+    // examples
+/*     const vegetables = ["Carrot", "Potato","Raddish"];
+     {
+        name: "aditya";
+        age:21;
+
+     }
+     const myFunction = function(){
+        console.log("this is function");
+     }
+*/
+
+    // is js statically or dynamically typed? 
+
+// let name = "aditya";
+// let age  = 21;
+// let isLoggedIn = false;
+// let state;
 
 // null => standalone value 
 // undefined => value is not defined 
 // typeof => gives the type of any data type 
-console.log(typeof undefined); // undefined
-console.log(typeof null); // object
+// console.log(typeof undefined); // undefined
+// console.log(typeof null); // object
+
+// Conversions
+// 33 => 33
+// "33abc" => NaN
+// true => 1; false => 0;
+
+// let someNumber = 18;
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// operations
+
+
+// console.log(typeof undefined);//undefined
+// console.log(typeof null);//object
+console.log(typeof Boolean);//
+console.log(typeof Number);
+console.log(typeof String);
+console.log(typeof Object);
+
+
+// console.log("1"+2); //12
+// console.log(1+3+"2"); //42
+// prefix and postfix 
+
+
+
