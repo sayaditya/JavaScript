7eccc1d241cf43bac6395c83126ddff4cbf8381a
Aditya Patel <96697492+sayaditya@users.noreply.github.com>
2024-09-30 17:59:08 +0000

    maths and numbers included

--- strings.js
+++ strings.js
@@ -1,5 +1,37 @@
-const name = "aditya";
-const repoCount = 5;
+// const name = "aditya";
+// const repoCount = 5;
 
-// console.log(name+repoCount+" Value "); //this is outdated
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+// // console.log(name+repoCount+" Value "); //this is outdated
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+// numbers
+
+// const score = 400;
+// console.log(score);
+
+// const balance = new Number(100);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2)); // majority time we use this in the ecommerce website to fix the price
+// const otherNumber = 999.894;
+// console.log(otherNumber.toPrecision(4));
+// const hundreds = 10000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// maths
+
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.round(4.7));
+// console.log(Math.ceil(8.1));
+// console.log(Math.floor(8.9));
+// console.log(Math.min(5,4,2,9));
+// console.log(Math.max(7,3,9,1));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 20;
+const max = 40;
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
