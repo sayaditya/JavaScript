4540d1c737005c403c704b75f3c6dc27c91b539e
Aditya Patel <96697492+sayaditya@users.noreply.github.com>
2024-09-30 19:33:37 +0000

    date and time completed

--- strings.js
+++ strings.js
@@ -27,11 +27,56 @@
 // console.log(Math.min(5,4,2,9));
 // console.log(Math.max(7,3,9,1));
 
-console.log(Math.random());
-console.log((Math.random()*10)+1);
-console.log(Math.floor(Math.random()*10)+1);
+// console.log(Math.random());
+// console.log((Math.random()*10)+1);
+// console.log(Math.floor(Math.random()*10)+1);
 
-const min = 20;
-const max = 40;
-console.log(Math.floor(Math.random()*(max-min+1))+min);
+// const min = 20;
+// const max = 40;
+// console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+// Date & time
+
+// let myDate = new Date();
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(typeof myDate); // it's an object -- may ask in interview
+
+// methods to represent date and time 
+
+// let newCreatedDate = new Date(2024,9,30);
+// let newCreatedDate = new Date(2024,9,30,10,5,6);
+// let newCreatedDate = new Date("2024-01-30");
+let newCreatedDate = new Date("08-30-2024");
+
+
+
+// console.log(newCreatedDate.toLocaleString());
+
+// if we need to get the dates with time and all the functions required
+
+let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+// console.log(newCreatedDate.getTime());
+// console.log(Date.now());
+
+// console.log(Math.floor(Date.now()/1000)); // asked in interview that to convert it into seconds --this is done by this method
+
+// if there is needed only basic date year and day 
+let myDate = new Date();
+console.log(myDate.getDate());
+console.log(myDate.getDay());
+console.log(myDate.getFullYear());
+
+// to get more customisation in localestring
+
+myDate.toLocaleString('default',{
+    weekday:"long",
+    day:"numeric",
+    hour:myTimeStamp
+
+})
 
