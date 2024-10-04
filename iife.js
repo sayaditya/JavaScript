d29b040d7532ba2c155ba7728532284e38a6a0b0
Aditya Patel <96697492+sayaditya@users.noreply.github.com>
2024-10-04 19:22:29 +0000

    iife done

--- iife.js
+++ iife.js
@@ -3,3 +3,8 @@
 (function connection(){
     console.log(` DB connected`)
 })()  // gobal scope k pollution se problem hoti hai aur us gobal scope k pollution ko hatane k liye we are using this iife
+
+
+
+
+
